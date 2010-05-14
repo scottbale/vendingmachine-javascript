4460c431a5a39b2f8830ac49f15b1d779db16262
Scott Bale <drbceb-tech@yahoo.com>
2010-05-13 22:49:35 -0500

    not sure I like this...

--- testsrc/TestAutomatedCashier.js
+++ testsrc/TestAutomatedCashier.js
@@ -1,18 +1,25 @@
 load('../src/Currency.js');
 load('../src/AutomatedCashier.js');
 
-var TEST_AUTOMATED_CASHIER = {
-    tests : {
-        testNickel : function(){
-          TESTER.assertEquals(5, CURRENCY.NICKEL);
-        },
-        testNickel2 : function(){
-          TESTER.assertEquals(4, CURRENCY.NICKEL);
-        }
-    }
-}
+var TEST_AUTOMATED_CASHIER = function(){
 
-TEST_LOADER.addTests(TEST_AUTOMATED_CASHIER.tests);
+    var module = {
+        tests : {
+            testNickel : function(){
+              TESTER.assertEquals(5, CURRENCY.NICKEL);
+            },
+            testNickel2 : function(){
+              TESTER.assertEquals(4, CURRENCY.NICKEL);
+            }
+        }
+    };
+    
+    TEST_LOADER.addTests(module.tests);
+
+    return module;
+
+}(TEST_LOADER);
+
 
 
 

eb72bd5f559fcf95da9c6404be0db34094d506f7
Scott Bale <drbceb-tech@yahoo.com>
2010-05-16 22:49:05 -0500

    protect private state and functions by making use of closure

--- testsrc/core/Tester.js
+++ testsrc/core/Tester.js
@@ -1,78 +1,82 @@
 var TESTER = function(CORE){
 
+    var makeTestRunner = function(test){
+
+        var testFunctions = [];
+        var testResults = {
+            ran : 0,
+            success : 0,
+            failure : 0,
+            error : 0,
+            failedTests : [],
+            failedMsgs : []
+        }
+
+
+        var loadFunctions = function(test){
+            for (prop in test){
+                if (typeof test[prop] == 'function'){
+                    if (prop.indexOf("test")>-1){
+                        testFunctions.push(test[prop]);
+                    }
+                } else if (typeof test[prop] == 'object'){
+                    loadFunctions(test[prop]);
+                }
+            }
+        };
+
+        loadFunctions(test);
+
+
+        var testRunner = {
+            test : function(){
+                for(i=0;i < testFunctions.length; i+=1){
+                    attemptTest(testFunctions[i]);
+                }
+                printResults();
+            }
+        };
+
+
+        var attemptTest = function(testFunction){
+            testResults.ran += 1;
+            try {
+                testFunction();
+                testResults.success += 1;
+            } catch (e) {
+                if (e.name.indexOf("assert") > -1){
+                    testResults.failure += 1;
+                    testResults.failedTests.push(testFunction);
+                    testResults.failedMsgs.push(e.message);
+                } else {
+                    testResults.error += 1;
+                }
+            }
+        }
+
+        var printResults = function(){
+            CORE.out("\n----------------------------------------------------");
+            CORE.out("\ntests:   " + testResults.ran );
+            CORE.out("\nsuccess: " + testResults.success );
+            CORE.out("\nfailure: " + testResults.failure );
+            CORE.out("\nerror:   " + testResults.error );
+
+            for(i=0;i < testResults.failure; i+=1){
+                CORE.out("\n----------------------------------------------------");
+                CORE.out("" + testResults.failedTests[i] );
+                CORE.out("message: " + testResults.failedMsgs[i] );
+            }
+
+            CORE.out("\n----------------------------------------------------\n\n");
+        }
+
+        return testRunner;
+    }
+
     var tester = {
 
         TestRunner : function(test){
-
-            var testFunctions = [];
-            var testResults = {
-                ran : 0,
-                success : 0,
-                failure : 0,
-                error : 0,
-                failedTests : [],
-                failedMsgs : []
-            }
-
-
-            var loadFunctions = function(test){
-                for (prop in test){
-                    if (typeof test[prop] == 'function'){
-                        if (prop.indexOf("test")>-1){
-                            testFunctions.push(test[prop]);
-                        }
-                    } else if (typeof test[prop] == 'object'){
-                        loadFunctions(test[prop]);
-                    }
-                }
-            };
-
-            loadFunctions(test);
-
-
-            var testRunner = {
-                test : function(){
-                    for(i=0;i < testFunctions.length; i+=1){
-                        attemptTest(testFunctions[i]);
-                    }
-                    printResults();
-                }
-            };
-
-
-            var attemptTest = function(testFunction){
-                testResults.ran += 1;
-                try {
-                    testFunction();
-                    testResults.success += 1;
-                } catch (e) {
-                    if (e.name.indexOf("assert") > -1){
-                        testResults.failure += 1;
-                        testResults.failedTests.push(testFunction);
-                        testResults.failedMsgs.push(e.message);
-                    } else {
-                        testResults.error += 1;
-                    }
-                }
-            }
-
-            var printResults = function(){
-                CORE.out("\n----------------------------------------------------");
-                CORE.out("\ntests:   " + testResults.ran );
-                CORE.out("\nsuccess: " + testResults.success );
-                CORE.out("\nfailure: " + testResults.failure );
-                CORE.out("\nerror:   " + testResults.error );
-
-                for(i=0;i < testResults.failure; i+=1){
-                    CORE.out("\n----------------------------------------------------");
-                    CORE.out("" + testResults.failedTests[i] );
-                    CORE.out("message: " + testResults.failedMsgs[i] );
-                }
-
-                CORE.out("\n----------------------------------------------------\n\n");
-            }
-
-            return testRunner;
+            return makeTestRunner(test);
         },
 
         assertEquals : function(oneArg, otherArg) {

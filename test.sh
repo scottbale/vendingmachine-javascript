210ff072fb3203f945e3aec875feba81970eb163
Scott Bale <drbceb-tech@yahoo.com>
2010-05-17 00:37:36 -0500

    add 2nd test class

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-java com.sun.tools.script.shell.Main src/rhino/rhino.js core/TestLoader vendingmachine/TestAutomatedCashier core/BootTests $@
+java com.sun.tools.script.shell.Main src/rhino/rhino.js core/TestLoader vendingmachine/TestCurrency vendingmachine/TestAutomatedCashier core/BootTests $@
---
+++ testsrc/vendingmachine/TestCurrency.js
@@ -0,0 +1,27 @@
+CORE.require('vendingmachine/Currency');
+
+(function(testLoader){
+
+    testLoader.addTests({
+        TestCurrency : {
+            testNickel : function(){
+              TESTER.assertEquals(5, CURRENCY.NICKEL);
+            },
+            testDime : function(){
+              TESTER.assertEquals(10, CURRENCY.DIME);
+            },
+            testQuarter : function(){
+              TESTER.assertEquals(25, CURRENCY.QUARTER);
+            }
+        }
+    });
+
+}(TEST_LOADER));
+
+
+
+
+
+
+
+
